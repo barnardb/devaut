d743716887d7abbc24b928bd8acab77330acd77c
Ben Barnard <barnardb@gmail.com>
2015-04-23 09:03:16 +0200

    Add colour and consistent error handling

--- src/main/bash/scripting/color.sh
+++ src/main/bash/scripting/color.sh
@@ -1,13 +1,85 @@
-function define_format {
-    eval "$(printf "function $1 { printf '%b%%s%b\\\\n' \"\$@\"; }" "$2" '\\E[0m')"
-}
-define_format black '\\E[30m'
-define_format red '\\E[31m'
-define_format green '\\E[32m'
-define_format yellow '\\E[33m'
-define_format blue '\\E[34m'
-define_format magenta '\\E[35m'
-define_format cyan '\\E[36m'
-define_format white '\\E[37m'
+# To include this in a file use something like the following:
+#     scripting_dir="$(dirname "$BASH_SOURCE")"
+#     source "${scripting_dir}/color.sh"
 
-function fail { red "$@" >&2; exit 1; }
+format() {
+    printf %b "\e[$1m"
+    if [ $# -gt 2 ]; then
+        printf '%s\n' "${@:3}"
+    else
+        cat
+    fi
+    printf %b "\e[$2m"
+}
+
+bold() {
+    format 1 22 "$@"
+}
+
+dim() {
+    format 2 22 "$@"
+}
+
+italic() {
+    format 3 23 "$@"
+}
+
+underline() {
+    format 4 24 "$@"
+}
+
+negative() {
+    format 7 27 "$@"
+}
+
+italic() {
+    format 3 23 "$@"
+}
+
+function define_colors() {
+    colors=(black red green yellow blue magenta cyan white)
+    declare -i c=0
+    while [ $c -lt "${#colors[@]}" ]; do
+        eval "${colors[$c]}() { format '3$c' 39 \"\$@\"; }"
+        eval "on-${colors[$c]}() { format '4$c' 49 \"\$@\"; }"
+        eval "bright-${colors[$c]}() { format '9$c' 39 \"\$@\"; }"
+        #eval "on-bright-${colors[$c]}() { format '10$c' 49 \"\$@\"; }"
+        c+=1
+    done
+}
+define_colors
+
+italic() {
+    format 3 23 "$@"
+}
+
+
+usage_error() {
+    bright-red "Error: $1"
+    usage
+    echo
+    printf '%s\n' "Use -? or --help for help" "${@:2}"
+    exit 2
+} >&2
+
+
+warn() {
+    yellow "$@"
+}
+
+fail() {
+    bright-red "Error: $1"
+    exit 1
+} >&2
+
+success() {
+    green "$@"
+}
+
+status() {
+    blue "$@"
+}
+
+prompt() {
+    read -p "$(bright-blue "$@")"
+}
