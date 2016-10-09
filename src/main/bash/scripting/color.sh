02e449bf748cde4709616bba941b988e38734f01
Ben Barnard <barnardb@gmail.com>
2016-10-09 08:53:35 -0400

    Simplify color.sh

--- src/main/bash/scripting/color.sh
+++ src/main/bash/scripting/color.sh
@@ -2,84 +2,46 @@
 #     scripting_dir="$(dirname "${BASH_SOURCE[0]}")"
 #     source "${scripting_dir}/color.sh"
 
-format() {
-    printf %b "\e[$1m"
-    if [ $# -gt 2 ]; then
-        printf '%s\n' "${@:3}"
-    else
-        cat
-    fi
-    printf %b "\e[$2m"
+function style_text {
+    case $# in
+        [0-1]) echo "usage: style_text <start-escape-code> <end-escape-code> [lines...]" >&2; return 2;;
+        2) lines=("$(cat)");;
+        *) lines=("${@:3}");;
+    esac
+    printf '%b%s%b\n' "\e[$1m" "${lines[@]}" "\e[$2m"
 }
 
-bold() {
-    format 1 22 "$@"
+function bold      { style_text 1 22 "$@"; }
+function dim       { style_text 2 22 "$@"; }
+function italic    { style_text 3 23 "$@"; }
+function underline { style_text 4 24 "$@"; }
+function negative  { style_text 7 27 "$@"; }
+
+colors=(black red green yellow blue magenta cyan white)
+for c in "${!colors[@]}"; do
+    eval "function ${colors[$c]}           { style_text 3$c 39 \"\$@\"; }"
+    eval "function on_${colors[$c]}        { style_text 4$c 49 \"\$@\"; }"
+    eval "function bright_${colors[$c]}    { style_text 9$c 39 \"\$@\"; }"
+    eval "function on_bright_${colors[$c]} { style_text 10$c 49 \"\$@\"; }"
+done
+
+function status  { blue "$@"; }
+function success { green "$@"; }
+function warn    { yellow "$@"; }
+
+function prompt {
+    read -r -p "$(bright_blue "$@")"
 }
 
-dim() {
-    format 2 22 "$@"
-}
+function fail {
+    bright_red "Error: $1"
+    exit 1
+} >&2
 
-italic() {
-    format 3 23 "$@"
-}
-
-underline() {
-    format 4 24 "$@"
-}
-
-negative() {
-    format 7 27 "$@"
-}
-
-italic() {
-    format 3 23 "$@"
-}
-
-function define_colors() {
-    colors=(black red green yellow blue magenta cyan white)
-    declare -i c=0
-    while [ $c -lt "${#colors[@]}" ]; do
-        eval "${colors[$c]}() { format '3$c' 39 \"\$@\"; }"
-        eval "on_${colors[$c]}() { format '4$c' 49 \"\$@\"; }"
-        eval "bright_${colors[$c]}() { format '9$c' 39 \"\$@\"; }"
-        #eval "on_bright_${colors[$c]}() { format '10$c' 49 \"\$@\"; }"
-        c+=1
-    done
-}
-define_colors
-
-italic() {
-    format 3 23 "$@"
-}
-
-
-usage_error() {
+function usage_error {
     bright_red "Error: $1"
     usage
     echo
     printf '%s\n' "Use -? or --help for help" "${@:2}"
     exit 2
 } >&2
-
-
-warn() {
-    yellow "$@"
-}
-
-fail() {
-    bright_red "Error: $1"
-    exit 1
-} >&2
-
-success() {
-    green "$@"
-}
-
-status() {
-    blue "$@"
-}
-
-prompt() {
-    read -r -p "$(bright_blue "$@")"
-}
