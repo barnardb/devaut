0921b6b0ff8727878a7e1171375baf35e77974d8
Ben Barnard <barnardb@gmail.com>
2019-11-21 22:59:09 +0100

    Add comments to help shellcheck find color.sh instead of ignoring SC1090

--- build/check-scripts.sh
+++ build/check-scripts.sh
@@ -30,5 +30,5 @@ find_scripts() {
         -print0
 }
 
-find_scripts      -perm -u+x | xargs -0 shellcheck --external-sources --exclude=SC2016,SC1090
-find_scripts -not -perm -u+x | xargs -0 shellcheck --external-sources --shell=bash --exclude=SC2016
+find_scripts      -perm -u+x | xargs -0 shellcheck --external-sources --exclude=SC2016
+find_scripts -not -perm -u+x | xargs -0 shellcheck --external-sources --exclude=SC2016 --shell=bash
